167a3959431ba908fc74042ed72f064feed60f09
rajibdas-it <rajibrad@gmail.com>
2024-09-28 02:14:43 +0600

    create authcontext and auth provider

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../Components/common/Header";
 
 const HomePage = () => {
-  return <div>homepage</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default HomePage;
