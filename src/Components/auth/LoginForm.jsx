d808e9d30d84018f4f8ab287f5d087e834b0cfd8
rajibdas-it <rajibrad@gmail.com>
2024-09-27 12:52:20 +0600

    login form design done

--- src/Components/auth/LoginForm.jsx
+++ src/Components/auth/LoginForm.jsx
@@ -10,6 +10,8 @@ const LoginForm = () => {
     formState: { errors },
   } = useForm();
 
+  console.log(errors);
+
   const formSubmit = (formData) => {
     console.log(formData);
   };
@@ -18,7 +20,7 @@ const LoginForm = () => {
       className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
       onSubmit={handleSubmit(formSubmit)}
     >
-      <Field label="Email">
+      <Field label="Email" error={errors.email}>
         <input
           {...register("email", { required: "Email is required" })}
           type="email"
@@ -29,7 +31,7 @@ const LoginForm = () => {
           }`}
         />
       </Field>
-      <Field label="Password">
+      <Field label="Password" error={errors.password}>
         <input
           {...register("password", { required: "Password is required" })}
           type="password"
@@ -39,6 +41,11 @@ const LoginForm = () => {
             !!errors.password ? "border-red-500" : "border-gray-200"
           }`}
         />
+      </Field>
+      <Field>
+        <button className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90">
+          Login
+        </button>
       </Field>
     </form>
   );
