b21ed89b8a0ec03089b4bc6b1c0ca1ef3cf6c7cd
rajibdas-it <rajibrad@gmail.com>
2024-09-27 11:35:09 +0600

    setup route with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
   return (
-    <div>
-      <h1>Rajib</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} exact />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/me" element={<ProfilePage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 };
 
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return <div>My Profile</div>;
+};
+
+export default ProfilePage;
