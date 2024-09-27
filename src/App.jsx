4d595bd358dce8433ce2b54589f40ba17bfd3068
rajibdas-it <rajibrad@gmail.com>
2024-09-27 12:05:28 +0600

    create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Login from "./pages/Login";
+import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import RegistrationPage from "./pages/RegistrationPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<HomePage />} exact />
       <Route path="/home" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/me" element={<ProfilePage />} />
       <Route path="/register" element={<RegistrationPage />} />
       <Route path="*" element={<NotFoundPage />} />
