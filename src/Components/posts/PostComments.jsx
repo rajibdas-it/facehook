7916f1d16c67107daa6522ed5f119d41bfe12b9f
rajibdas-it <rajibrad@gmail.com>
2024-10-02 02:46:05 +0600

    post component almost complete

--- src/Components/posts/PostComments.jsx
+++ src/Components/posts/PostComments.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAvatar } from "../../hook/useAvatar";
+import PostCommentsList from "./PostCommentsList";
 
-const PostComments = () => {
+const PostComments = ({ post }) => {
+  const { avatarURL } = useAvatar(post);
+  const [showComments, setShowComments] = useState(false);
+
   return (
     <div>
       {/* <!-- comment input box --> */}
       <div className="flex-center mb-3 gap-2 lg:gap-4">
         <img
           className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-          src="./assets/images/avatars/avatar_1.png"
+          src={avatarURL}
           alt="avatar"
         />
 
@@ -23,43 +28,14 @@ const PostComments = () => {
       </div>
       {/* <!-- comment filter button --> */}
       <div className="mt-4">
-        <button className="text-gray-300 max-md:text-sm">All Comment ▾</button>
+        <button
+          onClick={() => setShowComments(!showComments)}
+          className="text-gray-300 max-md:text-sm"
+        >
+          {!showComments ? "All Comment ▾" : "Hide Comment ▾"}
+        </button>
       </div>
-      {/* <!-- comments --> */}
-      <div className="space-y-4 divide-y divide-lighterDark pl-2 lg:pl-3">
-        {/* <!-- single comment --> */}
-        <div className="flex items-center gap-3 pt-4">
-          <img
-            className="max-w-6 max-h-6 rounded-full"
-            src="./assets/images/avatars/avatar_2.png"
-            alt="avatar"
-          />
-          <div>
-            <div className="flex gap-1 text-xs lg:text-sm">
-              <span>Tapas Adhikari: </span>
-              <span>Great Sumit Saha dada ❤</span>
-            </div>
-          </div>
-        </div>
-        {/* <!-- single comment ends --> */}
-
-        {/* <!-- single comment --> */}
-        <div className="flex items-center gap-3 pt-4">
-          <img
-            className="max-w-6 max-h-6 rounded-full"
-            src="./assets/images/avatars/avatar_1.png"
-            alt="avatar"
-          />
-          <div>
-            <div className="flex gap-1 text-xs lg:text-sm">
-              <span>Sumit Saha: </span>
-              <span>Great Sumit Saha dada ❤</span>
-            </div>
-          </div>
-        </div>
-        {/* <!-- single comment ends --> */}
-      </div>
-      {/* <!-- comments ends --> */}
+      {showComments && <PostCommentsList comments={post?.comments} />}
     </div>
   );
 };
---
+++ src/Components/posts/PostCommentsList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const PostCommentsList = ({ comments }) => {
+  return (
+    <>
+      <div className="space-y-4 divide-y divide-lighterDark pl-2 lg:pl-3">
+        {comments &&
+          comments.map((comment) => (
+            <div key={comment?.id} className="flex items-center gap-3 pt-4">
+              <img
+                className="max-w-6 max-h-6 rounded-full"
+                src={`${import.meta.env.VITE_SERVER_BASE_URL}/${
+                  comment?.author?.avatar
+                }`}
+                alt="avatar"
+              />
+              <div>
+                <div className="flex gap-1 text-xs lg:text-sm">
+                  <span>{comment?.author?.name}: </span>
+                  <span>{comment?.comment}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default PostCommentsList;
