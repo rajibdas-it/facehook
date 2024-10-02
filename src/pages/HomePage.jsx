e2cebe78f65e16ef1a98c42ae4b925391c8f64a8
rajib <rajibrad@gmail.com>
2024-10-02 17:27:52 +0600

    home page post views

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-import Header from "../Components/common/Header";
+import React, { useEffect, useReducer } from "react";
 import { useAuth } from "../hook/useAuth";
+import { initialState, postReducer } from "../reducers/PostReducer";
+import useAxios from "../hook/useAxios";
+import PostList from "../Components/posts/PostList";
+import { actions } from "../actions";
 
 const HomePage = () => {
-  const { auth } = useAuth();
-  // console.log(auth);
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+  }, []);
+
   return (
     <div>
-      <h1>Homepage</h1>
+      <PostList />
     </div>
   );
 };
