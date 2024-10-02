45f1f19539d5df45289412d14502fa823b13032a
rajib <rajibrad@gmail.com>
2024-10-02 17:58:39 +0600

    home page posts show

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useReducer } from "react";
-import { useAuth } from "../hook/useAuth";
+
 import { initialState, postReducer } from "../reducers/PostReducer";
 import useAxios from "../hook/useAxios";
 import PostList from "../Components/posts/PostList";
@@ -11,11 +12,36 @@ const HomePage = () => {
 
   useEffect(() => {
     dispatch({ type: actions.post.DATA_FETCHING });
+
+    const fetchPosts = async () => {
+      try {
+        const res = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+
+        if (res.status === 200) {
+          dispatch({ type: actions.post.DATA_FETCHED, data: res.data });
+        }
+      } catch (error) {
+        console.error(error);
+        dispatch({ type: actions.post.DATA_FETCH_ERROR, error: error.message });
+      }
+    };
+
+    fetchPosts();
   }, []);
+  console.log(state?.posts);
+
+  if (state?.loading) {
+    return <p>Loading</p>;
+  }
+  if (state?.error) {
+    return <p>Error in fetching</p>;
+  }
 
   return (
     <div>
-      <PostList />
+      <PostList posts={state?.posts} />
     </div>
   );
 };
