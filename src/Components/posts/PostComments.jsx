bf381057dde96dd981b8bb2305dfe6d440d31081
rajibdas-it <rajibrad@gmail.com>
2024-10-04 01:17:38 +0600

    incomplete finished

--- src/Components/posts/PostComments.jsx
+++ src/Components/posts/PostComments.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from "react";
 import { useAvatar } from "../../hook/useAvatar";
 import PostCommentsList from "./PostCommentsList";
+import useAxios from "../../hook/useAxios";
+import { useAuth } from "../../hook/useAuth";
 
 const PostComments = ({ post }) => {
+  const { auth } = useAuth();
   const { avatarURL } = useAvatar(post);
   const [showComments, setShowComments] = useState(false);
 
-  const [comments, setComments] = useState();
+  const [comments, setComments] = useState(post?.comments);
+  const [comment, setComment] = useState("");
+
+  const { api } = useAxios();
+
+  const addComment = async (e) => {
+    const keyCode = e.keyCode;
+    if (keyCode === 13) {
+      // ei 13 holo enter button er key code
+      try {
+        const res = await api.patch(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id}/comment`,
+          { comment }
+        );
+        if (res.status === 200) {
+          setComments([...res.data.comments]);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <div>
@@ -14,7 +38,7 @@ const PostComments = ({ post }) => {
       <div className="flex-center mb-3 gap-2 lg:gap-4">
         <img
           className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-          src={avatarURL}
+          src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}`}
           alt="avatar"
         />
 
@@ -24,6 +48,9 @@ const PostComments = ({ post }) => {
             className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
             name="post"
             id="post"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            onKeyDown={(e) => addComment(e)} //button chara enter dilei kaj korbe. er jonno onKeyDown
             placeholder="What's on your mind?"
           />
         </div>
@@ -37,7 +64,7 @@ const PostComments = ({ post }) => {
           {!showComments ? "All Comment ▾" : "Hide Comment ▾"}
         </button>
       </div>
-      {showComments && <PostCommentsList comments={post?.comments} />}
+      {showComments && <PostCommentsList comments={comments} />}
     </div>
   );
 };
